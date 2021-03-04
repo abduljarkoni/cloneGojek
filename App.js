0771a301eee6043bc0af8af9d5b7326d182f2baa
abdul jarkoni <abduljarkoni.aj@gmail.com>
2021-03-04 17:23:19 +0700

    new internal gojek

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {StyleSheet, TextInput, View, Text, Image} from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 // import home from './icon/home.png';
 import order from './icon/order.png';
 import help from './icon/help.png';
@@ -11,9 +19,8 @@ const App = () => {
     <>
       <View style={{flex: 1}}>
         {/* flex1 akan memakan sisa dari tinggai view yang di bawahnya */}
-        <View style={{flex: 1, backgroundColor: 'white'}}>
-          {/* section search/pencarian */}
-
+        <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
+          {/* 2.section search/pencarian */}
           <View
             style={{
               marginHorizontal: 17,
@@ -62,7 +69,7 @@ const App = () => {
             </View>
           </View>
 
-          {/* section gopay */}
+          {/*3. section gopay */}
           {/* wrapper/pembungkus global */}
           <View
             style={{
@@ -176,7 +183,7 @@ const App = () => {
             </View>
           </View>
 
-          {/* main/halaman icon menu utama feature   menu gajek transportasi dll*/}
+          {/* 4. main/halaman icon menu utama feature   menu gajek transportasi dll*/}
           {/* flexwrapp:wrap adalah agar membuat objec/component turun kebawah */}
 
           <View
@@ -410,7 +417,258 @@ const App = () => {
               </View>
             </View>
           </View>
-        </View>
+          {/* garis abu abu pembatas section */}
+          <View
+            style={{
+              height: 17,
+              backgroundColor: '#F2F2F4',
+              marginTop: 10,
+            }}></View>
+
+          {/* new sction/halaman berita */}
+          <View style={{paddingTop: 16, paddingHorizontal: 16}}>
+            {/* agar image tida keluar dari wrapper/pembungkusnya */}
+            <View style={{position: 'relative'}}>
+              <Image
+                source={require('./news/bola.jpeg')}
+                style={{width: '100%', height: 170, borderRadius: 7}}
+              />
+
+              {/* //membuat bayangan hitam agar konten diatas nya /lognya bisa terlihat */}
+              <View
+                style={{
+                  position: 'absolute',
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: 'black',
+                  left: 0,
+                  top: 0,
+                  borderRadius: 7,
+                  opacity: 0.2,
+                }}></View>
+              <View
+                style={{
+                  height: 15,
+                  width: 60,
+                  position: 'absolute',
+                  top: 16,
+                  left: 16,
+                  borderRadius: 2,
+                }}>
+                <Image
+                  source={require('./icon/logogojek.png')}
+                  style={{
+                    height: '100%',
+                    width: '100%',
+                    borderRadius: 2,
+                  }}
+                />
+              </View>
+            </View>
+
+            {/* membuat garis didalam wrapper pembungkus button menggunakan  style */}
+            {/* borderBottomColor: '#E8E9ED' borderBottomWidth: 1,marginBottom: 20, */}
+            <View
+              style={{
+                paddingTop: 10,
+                paddingBottom: 20,
+                borderBottomColor: '#E8E9ED',
+                borderBottomWidth: 1,
+              }}>
+              <Text
+                style={{fontSize: 16, fontWeight: 'bold', color: '#1c1c1c'}}>
+                GO-NEWS
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  fontWeight: 'normal',
+                  color: '#7A7A7A',
+                  marginBottom: 11,
+                }}>
+                Selamat Indonesia Menang, Dalam Piala Dunia 2029
+              </Text>
+
+              {/* membuat button custome dengan view */}
+              {/* alignSelf ada properti yang di gunakn untuk button
+              agar panjang button mengikuti panjang teks di dalamnya
+              alignselft : "center" agar button ke tengah
+              aligselft : "flex-end" agar button ke sebelah kanan/di akhir
+              aligselft : "stretch" agar button memenuhi sisaspace wrappernya/view 
+              */}
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#61A756',
+                  paddingHorizontal: 12,
+                  paddingVertical: 11,
+                  alignSelf: 'flex-end',
+                  borderRadius: 5,
+                }}>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    textAlign: 'center',
+                  }}>
+                  READ
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          {/* 5.section banner information internal gojek */}
+          <View style={{padding: 16}}>
+            <View style={{height: 25, width: 70}}>
+              <Image
+                source={require('./icon/logogojek.png')}
+                style={{
+                  height: '100%',
+                  width: '100%',
+                  borderRadius: 2,
+                }}
+              />
+            </View>
+            <Text style={{fontSize: 17, fontWeight: 'bold'}}>
+              Complete your profile
+            </Text>
+            <View>
+              <Image
+                source={require('./icon/fb.png')}
+                style={{width: 100, height: 100}}
+              />
+              <View>
+                <Text>Connect With Facebook</Text>
+                <Text>Login faster without verification code</Text>
+              </View>
+            </View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#61A756',
+                paddingHorizontal: 12,
+                paddingVertical: 11,
+                alignSelf: 'flex-end',
+                borderRadius: 5,
+              }}>
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: 'bold',
+                  color: 'white',
+                  textAlign: 'center',
+                }}>
+                CONNECT
+              </Text>
+            </TouchableOpacity>
+            <View
+              style={{
+                marginTop: 16,
+                marginBottom: 16,
+                borderBottomColor: '#E8E9ED',
+                borderBottomWidth: 1,
+              }}></View>
+          </View>
+
+          {/* 6.section banner goofod/halaman banner gofood */}
+          <View style={{padding: 16}}>
+            {/* setiap position absolute harus dibungkus dengan wrapper/view dengan 
+            position relative agar objek/componen tidak keluar dari pembungkusnya */}
+
+            <View
+              style={{
+                position: 'relative',
+              }}>
+              <Image
+                source={require('./news/makanan.jpeg')}
+                style={{width: '100%', height: 170, borderRadius: 7}}
+              />
+
+              {/* membuat bayangan hitam/transparan yg menutupi gambar dengan position
+              absolute */}
+              <View
+                style={{
+                  position: 'absolute',
+                  width: '100%',
+                  height: '100%',
+                  backgroundColor: 'black',
+                  left: 0,
+                  top: 0,
+                  borderRadius: 7,
+                  opacity: 0.2,
+                }}></View>
+              <Image
+                source={require('./news/gofood.png')}
+                style={{
+                  height: 25,
+                  width: 75,
+                  position: 'absolute',
+                  top: 16,
+                  left: 16,
+                  borderRadius: 5,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 16,
+                left: 16,
+                width: '100%',
+                borderRadius: 5,
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingHorizontal: 16,
+                paddingVertical: 16,
+                marginBottom: 32,
+              }}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    color: 'white',
+                    fontWeight: 'bold',
+                    marginBottom: 5,
+                  }}>
+                  free GO-FOOD Voucher
+                </Text>
+                <Text style={{fontSize: 12, color: 'white', fontWeight: '400'}}>
+                  Quick,before they run out !
+                </Text>
+              </View>
+              <View style={{flex: 1, paddingLeft: 12}}>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#61A756',
+                    paddingHorizontal: 12,
+                    paddingVertical: 11,
+                    alignSelf: 'stretch',
+                    borderRadius: 5,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontWeight: 'bold',
+                      color: 'white',
+                      textAlign: 'center',
+                    }}>
+                    GET VOUCHER
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View
+              style={{
+                marginTop: 16,
+                marginBottom: 16,
+                borderBottomColor: '#E8E9ED',
+                borderBottomWidth: 1,
+              }}></View>
+          </View>
+        </ScrollView>
+
+        {/* 1. navigasi utama gojek  navbar aplikasinya yang fixed/tetap*/}
+        {/* scrollview berada di atas navbar agar navbar tidak ikut ke scroll saat di geser2 */}
+
         <View
           style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
           <View
